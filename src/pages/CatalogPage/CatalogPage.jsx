7e612b932c914fa5b1e802b5351237d050397eed
julia <julia.zhyvylo@asila.mk>
2025-03-19 15:38:34 +0100

    test mui

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,7 +1,14 @@
+import Box from '@mui/material/Box';
+import SearchBox from '../../components/SearchBox/SearchBox';
+import TrucksList from '../../components/TrucksList/TrucksList';
+
 
 function CatalogPage() {
   return (
-    <div>CatalogPage</div>
+    <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+      <SearchBox />
+      <TrucksList />
+    </Box>
   )
 }
 
