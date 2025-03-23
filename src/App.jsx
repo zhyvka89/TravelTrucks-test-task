567959c8c0753609874e72e15c04712d88100c6f
julia <julia.zhyvylo@asila.mk>
2025-03-23 20:42:30 +0100

    add mising changes

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,88 @@ const theme = createTheme({
   },
   palette: {
     primary: {
-      main: '#e44848',
+      main: "#E44848", // Main red
+      dark: "#D84343", // Darker red
+      light: "#FFC531", // Accent yellow
     },
+    secondary: {
+      main: "#475467", // Dark gray-blue
+    },
+    text: {
+      primary: "#101828", // Dark text
+      secondary: "#6C717B", // Lighter gray
+    },
+    background: {
+      default: "#F2F4F7", // Main background
+      inputs: "#F7F7F7",
+      paper: "#FFFFFF", // Card background
+    },
+    divider: "#DADDE1", // Border lines
+    action: {
+      hover: "#F7F7F7", // Hover background
+    },
+  },
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#f7f7f7',
+        },
+      },
+    },
+    MuiContainer: {
+      styleOverrides: {
+        maxWidthLg: {
+          paddingLeft: 64,
+          paddingRight: 64
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: '100px',
+          padding: '16px 60px'
+        }
+      }
+    },
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#f7f7f7',
+          borderRadius: '12px',
+          height: '56px'
+        }
+      }
+    },
+    MuiToggleButtonGroup: {
+      styleOverrides: {
+        grouped: {
+          display: 'flex',
+          flexDirection: 'column',
+          width: '112px',
+          border:'1px solid #DADDE1',
+          borderRadius: '12px',
+          padding: '16px 18px',
+          "&.Mui-selected": {
+            border:'1px solid #E44848',
+            backgroundColor: 'transparent',
+            color: '#101828'
+          },
+          "&:hover": {
+            border:'1px solid #E44848'
+          }
+        },
+  
+      }
+    }
+  },
+  typography: {
+    fontFamily: 'Inter, sans-serif',
+    fontSize: 14,
+    fontWeightBold: 500,
+    lineHeight: 1.5,
+    button: {textTransform: 'none'}
   },
 });
 
